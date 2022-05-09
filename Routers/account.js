bb287125599b8c21ea36890a518324abf1489e3a
LilThawg <72289126+LilThawg@users.noreply.github.com>
2022-05-09 12:28:35 +0700

    Update account.js

--- Routers/account.js
+++ Routers/account.js
@@ -5,4 +5,4 @@ router.post("/register", accountControllers.REGISTER);
 router.post("/login", accountControllers.LOGIN);
 router.get("/logout", accountControllers.LOGOUT);
 
-module.exports = router;git add README.md
+module.exports = router;
