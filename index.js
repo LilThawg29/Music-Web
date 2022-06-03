eaffada2143d295334c388e0c20213b7f3e9f8b5
LilThawg <72289126+LilThawg@users.noreply.github.com>
2022-06-03 16:04:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ app.use("/api/list-music", listMusicRouter);
 app.use("/api/play-history", playHistoryRouter);
 app.use("/api/search", searchRouter);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running");
 });
